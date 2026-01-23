d2bafdf0bba42cfa6a560b6ebc5841d5d5b65492
Hyunsu Lim <karegoie@snu.ac.kr>
2026-01-23 15:38:40 +0900

    Publish gh-pages

---
+++ sing-web/pkg/sing_web_bg.wasm.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_mappingresult_free: (a: number, b: number) => void;
+export const __wbg_get_mappingresult_bam_data: (a: number) => [number, number];
+export const __wbg_set_mappingresult_bam_data: (a: number, b: number, c: number) => void;
+export const __wbg_singwebengine_free: (a: number, b: number) => void;
+export const singwebengine_new: () => number;
+export const singwebengine_process_reference_chunk: (a: number, b: number, c: number, d: number) => [number, number];
+export const singwebengine_export_index: (a: number) => [number, number, number, number];
+export const singwebengine_import_index: (a: number, b: number, c: number) => [number, number];
+export const singwebengine_run_mapping_chunk: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => [number, number, number];
+export const singwebengine_get_total_reads: (a: number) => number;
+export const singwebengine_get_mapped_reads: (a: number) => number;
+export const singwebengine_get_mapped_bases: (a: number) => number;
+export const singwebengine_get_genome_size: (a: number) => number;
+export const singwebengine_get_avg_coverage: (a: number) => number;
+export const singwebengine_map_reads_chunk: (a: number, b: number, c: number) => [number, number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
