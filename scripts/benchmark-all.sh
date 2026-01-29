51adbd12266cc6d34f3df8fd5bdbb3eedee8e189
Hyunsu Lim <karegoie@snu.ac.kr>
2026-01-29 09:04:17 +0900

    add brassica benchmark

--- scripts/benchmark-all.sh
+++ scripts/benchmark-all.sh
@@ -4,10 +4,12 @@ set -e
 echo "cd `pwd`; bash scripts/benchmark.sh y" | qsub -N benchmark-y -l nodes=node04:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-y.log
 echo "cd `pwd`; bash scripts/benchmark.sh a" | qsub -N benchmark-a -l nodes=node04:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-a.log
 echo "cd `pwd`; bash scripts/benchmark.sh m" | qsub -N benchmark-m -l nodes=node04:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-m.log
+echo "cd `pwd`; bash scripts/benchmark.sh b" | qsub -N benchmark-b -l nodes=node04:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-b.log
 echo "cd `pwd`; bash scripts/benchmark.sh h" | qsub -N benchmark-h -l nodes=node04:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-h.log
 echo "cd `pwd`; bash scripts/benchmark-gatk.sh y" | qsub -N benchmark-gy -l nodes=node03:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-gy.log
 echo "cd `pwd`; bash scripts/benchmark-gatk.sh a" | qsub -N benchmark-ga -l nodes=node03:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-ga.log
 echo "cd `pwd`; bash scripts/benchmark-gatk.sh m" | qsub -N benchmark-gm -l nodes=node03:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-gm.log
+echo "cd `pwd`; bash scripts/benchmark-gatk.sh b" | qsub -N benchmark-gb -l nodes=node03:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-gb.log
 echo "cd `pwd`; bash scripts/benchmark-gatk.sh h" | qsub -N benchmark-gh -l nodes=node03:ppn=8 -v WORKDIR=`pwd` -j oe -o ~/log/benchmark-gh.log
 
 echo "ALL JOBS SUMBMITTED!"
